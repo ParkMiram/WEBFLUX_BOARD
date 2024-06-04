6ee7072675f972dab4b115bed3ad12d3ce05adf0
ParkMiram <myurm@naver.com>
2024-06-04 17:55:20 +0900

    [feat] db 연동

--- src/main/java/org/example/webflux_board/DatabaseConfig.java
+++ src/main/java/org/example/webflux_board/DatabaseConfig.java
@@ -6,22 +6,32 @@ import io.r2dbc.spi.ConnectionFactoryOptions;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.AbstractR2dbcConfiguration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import reactor.core.publisher.Flux;
 
-@Configuration
-@EnableR2dbcAuditing
-public class DatabaseConfig extends AbstractR2dbcConfiguration {
-    @Override
+//@Configuration
+//@EnableR2dbcAuditing
+public class DatabaseConfig {
+//        extends AbstractR2dbcConfiguration {
+//    @Override
     public ConnectionFactory connectionFactory() {
+//        return ConnectionFactories.get(
+//            ConnectionFactoryOptions.builder()
+//                .option(ConnectionFactoryOptions.DRIVER, "oracle")
+//                .option(ConnectionFactoryOptions.HOST, "localhost")
+//                .option(ConnectionFactoryOptions.PORT, 1521)
+//                .option(ConnectionFactoryOptions.DATABASE, "boards")
+//                .option(ConnectionFactoryOptions.USER, "system")
+//                .option(ConnectionFactoryOptions.PASSWORD, "oracle")
+//                .build()
+//        );
         return ConnectionFactories.get(
-            ConnectionFactoryOptions.builder()
-                .option(ConnectionFactoryOptions.DRIVER, "oracle")
-                .option(ConnectionFactoryOptions.HOST, "localhost")
-                .option(ConnectionFactoryOptions.PORT, 1521)
-                .option(ConnectionFactoryOptions.DATABASE, "boards")
-                .option(ConnectionFactoryOptions.USER, "root")
-                .option(ConnectionFactoryOptions.PASSWORD, "1234")
-                .build()
+                ConnectionFactoryOptions.builder()
+                        .option(ConnectionFactoryOptions.DRIVER, "mysql")
+                        .option(ConnectionFactoryOptions.HOST, "localhost")
+                        .option(ConnectionFactoryOptions.PORT, 3306)
+                        .option(ConnectionFactoryOptions.DATABASE, "webflux")
+                        .option(ConnectionFactoryOptions.USER, "root")
+                        .option(ConnectionFactoryOptions.PASSWORD, "1234")
+                        .build()
         );
     }
 }
